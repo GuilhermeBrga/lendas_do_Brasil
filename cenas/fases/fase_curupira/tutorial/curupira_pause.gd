6c47acab5b7f73188f89353d8101284374b4ab11
AlvesAlbano <samir.saar@gmail.com>
2025-11-26 23:49:34 -0300

    add: logica de pausar

---
+++ cenas/fases/fase_curupira/tutorial/curupira_pause.gd
@@ -0,0 +1,19 @@
+extends Control
+
+signal mudar_sprite_btn
+func _ready() -> void:
+	#$TextureRect/Button.disabled = true
+	pass
+
+func _on_button_pressed() -> void:
+	visible = false
+	pausar(false)
+	mudar_sprite_btn.emit()
+
+func pausar(pausado:bool):
+	get_tree().paused = pausado
+
+func _on_curupirapause_aparecer_pop_up_pause() -> void:
+	visible = not visible
+	$TextureRect/Button.disabled = false
+	pausar(true)
--- cenas/fases/fase_curupira/tutorial/tutorial_curupira.gd
+++ cenas/fases/fase_curupira/tutorial/tutorial_curupira.gd
@@ -1,14 +1,11 @@
 extends Control
 
-signal pausado
-
 func _ready() -> void:
 	pausar(true)
 
 func _on_button_pressed() -> void:
 	pausar(false)
 	visible = false
-	pausado.emit()
 
 func pausar(pausado:bool):
 	get_tree().paused = pausado
